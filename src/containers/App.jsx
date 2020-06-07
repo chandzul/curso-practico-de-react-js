5dcbc282993349957363e302fb99721965a7b6a8
chandzul <dchandzul@gmail.com>
2020-06-07 06:45:29 -0500

    only static

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -21,24 +21,34 @@ const App = () => {
       <Header />
       <Search />
 
-      {initialState.mylist.length > 0 && 
+      {/* {initialState.mylist.length > 0 && 
         <Categories title="Mi lista">
           <Carousel>
             <CarouselItem />
           </Carousel>
         </Categories>
-      }
+      } */}
   
-      <Categories title="Tendencias">
+      {/* <Categories title="Tendencias">
         <Carousel>
           {initialState.trends.map(item =>
             <CarouselItem key={item.id} {...item}/>
           )}
         </Carousel>
-      </Categories>
+      </Categories> */}
   
       <Categories title="Originales de Platzi Video">
         <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
           <CarouselItem />
         </Carousel>
       </Categories>
